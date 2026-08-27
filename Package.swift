fabeef8ea153d3b67af59732db517401574d36f3
Usman N <usman.nazir@guardian.co.uk>
2026-08-27 22:30:41 +0100

    Rename package

--- Package.swift
+++ Package.swift
@@ -4,22 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "calendar-ios",
+    name: "Calendar-iOS",
     platforms: [
         .iOS(.v18)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "calendar-ios",
-            targets: ["calendar-ios"]
+            name: "Calendar-iOS",
+            targets: ["Calendar-iOS"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "calendar-ios"
+            name: "Calendar-iOS"
         ),
 
     ],
